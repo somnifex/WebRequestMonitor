f9b1085f637ea3e68425c2a74ee6844bfc3d41bc
HowieWood <98788152+utopeadia@users.noreply.github.com>
2025-02-21 18:43:44 +0800

    Icon closer to the edge

--- webrequestmonitor.user.js
+++ webrequestmonitor.user.js
@@ -2,7 +2,7 @@
 // @name         网页请求监视器
 // @icon         data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100' width='100' height='100' style='overflow: visible'%3E%3Ctext x='50%' y='60%' font-size='60' text-anchor='middle' dominant-baseline='middle'%3E🌍%3C/text%3E%3C/svg%3E
 // @namespace    http://tampermonkey.net/
-// @version      0.1.4
+// @version      0.1.5
 // @description  Web Request Monitor
 // @author       Howie Wood
 // @match        *://*/*
@@ -36,7 +36,7 @@
         #net-monitor-btn {
             position: fixed;
             bottom: 95px;
-            right: 14px;
+            right: 5px;
             z-index: 9999;
             background: linear-gradient(135deg, var(--primary-color), var(--secondary-color));
             color: white;
