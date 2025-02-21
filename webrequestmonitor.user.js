a9b5cf8cf742a31b4e155b5ed5dce5477e2f3e8d
HowieWood <98788152+utopeadia@users.noreply.github.com>
2025-02-21 21:02:00 +0800

    Fixed a bug that affected usage, can be used as a formal release

--- webrequestmonitor.user.js
+++ webrequestmonitor.user.js
@@ -2,7 +2,7 @@
 // @name         网页请求监视器
 // @icon         data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100' width='100' height='100' style='overflow: visible'%3E%3Ctext x='50%' y='60%' font-size='60' text-anchor='middle' dominant-baseline='middle'%3E🌍%3C/text%3E%3C/svg%3E
 // @namespace    http://tampermonkey.net/
-// @version      0.1.8
+// @version      1.0.0
 // @description  Web Request Monitor
 // @author       Howie Wood
 // @match        *://*/*
